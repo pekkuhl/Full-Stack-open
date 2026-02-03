f514589dbc2e2d11b98a5207c1b7c5fdc3548cba
Pekka <184255148+pekkuhl@users.noreply.github.com>
2026-02-03 14:53:45 +0200

    kurssitiedot step2

--- part1/kurssitiedot/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/kurssitiedot/src/App.jsx
@@ -5,19 +5,21 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return(
+      <p>
+        {props.part} {props.exercises}
+      </p>
+      )
+}
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-            <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
   )
 }
 
@@ -26,8 +28,6 @@ const Total = (props) => {
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
-
-
 
 
 const App = () => {
@@ -46,6 +46,7 @@ const App = () => {
       part1={part1} exercises1={exercises1}
       part2={part2} exercises2={exercises2}
       part3={part3} exercises3={exercises3}/>
+
       <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
