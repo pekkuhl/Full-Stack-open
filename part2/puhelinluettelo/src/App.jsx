ebab09b5e6ad654921a3be300f9313f482731446
Pekka <184255148+pekkuhl@users.noreply.github.com>
2026-02-05 11:30:04 +0200

    2.11 puhelinluettelo step 6

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from "./components/Filter.jsx"
 import Persons from "./components/Persons.jsx"
 import PersonForm from "./components/PersonForm.jsx"
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterString, setFilterString] = useState("")
+
+  const getPersons = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(res=> setPersons(res.data))
+  }
+
+  useEffect(getPersons,[])
 
   const handleNameChange = (e) => {
     console.log(e.target.value)
