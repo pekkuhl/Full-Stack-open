f49a18c4e9f1745d50a67abe70a8449d269694cd
Pekka <184255148+pekkuhl@users.noreply.github.com>
2026-02-09 08:37:09 +0200

    puhelinluettelo backend step 9

--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', getBody = (req) => {
@@ -8,6 +9,7 @@ morgan.token('body', getBody = (req) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 
 let persons = [
