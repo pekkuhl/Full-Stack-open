5c0f025e7ab75943f69dab40c227fe2b8d9d957c
Pekka <184255148+pekkuhl@users.noreply.github.com>
2026-03-16 10:47:45 +0200

    6.20

---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,10 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Fetching anecdotes failed')
+  }
+  return await response.json()
+}
