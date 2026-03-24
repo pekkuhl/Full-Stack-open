c358befd2c6a90c3ea94f78dc3cc4ab505e39489
Pekka <184255148+pekkuhl@users.noreply.github.com>
2026-03-24 12:58:51 +0200

    7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -99,6 +99,13 @@ const CreateNew = ({addNew, showNotification}) => {
     showNotification(`a new anecdote ${content.value} created!`)
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -116,6 +123,7 @@ const CreateNew = ({addNew, showNotification}) => {
           <input {...info}/>
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
